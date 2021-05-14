62d47738098757b2ef05d1784a22feca75dedea8
venu9398 <venuvaragani@gmail.com>
2021-05-14 14:13:14 +0530

    routing is established

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import{FormsModule}  from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductdetailsComponent } from './productdetails/productdetails.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegistrationComponent,
     ProductsComponent,
     FooterComponent,
-    ProductdetailsComponent
+    ProductdetailsComponent,
+    HomeComponent,
+    LoginComponent,
+    ContactusComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
